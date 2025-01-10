0a9044d7773c86d5365a8b5bcc0cc12e1af8af90
Srikanthreddy140 <147992962+Srikanthreddy140@users.noreply.github.com>
2025-01-10 10:56:16 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,9 +18,9 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'TechVJBot')
-API_ID = int(environ.get('API_ID', '29002304'))
-API_HASH = environ.get('API_HASH', '94e59f4efbb74820d043de101f6adc6d')
-BOT_TOKEN = environ.get('BOT_TOKEN', "6327892142:AAH-Mc4LLwB6eQh5_hujFAb-gyrkTXNHFqs")
+API_ID = int(environ.get('API_ID', '21180955'))
+API_HASH = environ.get('API_HASH', '5006e4600663a88cd7cb42617b1bb61a)
+BOT_TOKEN = environ.get('BOT_TOKEN', "8093755719:AAGMXyycQdgbWhBytQrr3iaTLUtDpttFzXU")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 1800))
@@ -30,8 +30,8 @@ MELCOW_VID = environ.get("MELCOW_VID", "https://t.me/howtoopenlinkssri/16")
 SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5cbb8.jpg")
 
 # Admins, Channels & Users
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002014827868'))
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6139556802').split()]
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002270564015'))
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7724838876').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002075397287').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
