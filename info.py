d4490ec3edd15e22296818d3312197b54e398f7d
Srikanthreddy140 <147992962+Srikanthreddy140@users.noreply.github.com>
2025-01-11 21:48:16 +0530

    Update info.py

--- info.py
+++ info.py
@@ -20,7 +20,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'TechVJBot')
 API_ID = int(environ.get('API_ID', '21180955'))
 API_HASH = environ.get('API_HASH', '5006e4600663a88cd7cb42617b1bb61a')
-BOT_TOKEN = environ.get('BOT_TOKEN', "8093755719:AAGMXyycQdgbWhBytQrr3iaTLUtDpttFzXU")
+BOT_TOKEN = environ.get('BOT_TOKEN', "8093755719:AAGP3NeAJAhAdtQGnrr4tL3tcX8OB2kDi1M")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 1800))
@@ -51,7 +51,7 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-100
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI',"mongodb+srv://royalsrikanth140:n0O97ntpssfJWUjX@cluster0.788gy1y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI',"mongodb+srv://ROYALBOMMA:ROYALBOMMA766@royalbomma.bomyg.mongodb.net/?retryWrites=true&w=majority&appName=ROYALBOMMA")
 DATABASE_NAME = environ.get('DATABASE_NAME', "royalsrikanth140")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'ROYALBOMMA')
 
@@ -67,7 +67,7 @@ OWNER_USERNAME = environ.get('OWNER_USERNAME', 'Royalbomma143') # owner username
 
 # Clone Information : If Clone Mode Is True Then Bot Clone Other Bots.
 CLONE_MODE = bool(environ.get('CLONE_MODE', True)) # Set True or False
-CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "mongodb+srv://royalsrikanth140:n0O97ntpssfJWUjX@cluster0.788gy1y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # Necessary If clone mode is true
+CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "mongodb+srv://ROYALBOMMA:ROYALBOMMA766@royalbomma.bomyg.mongodb.net/?retryWrites=true&w=majority&appName=ROYALBOMMA") # Necessary If clone mode is true
 PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', 'ROYALBOMMA') # Public Channel Username Without @ or without https://t.me/ and Bot Is Admin With Full Right.
 
 # Links
