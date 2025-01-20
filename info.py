da0bbf10399c98f6957fdf99d48c2f6ee42204a2
Srikanthreddy140 <147992962+Srikanthreddy140@users.noreply.github.com>
2025-01-20 14:07:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -51,7 +51,7 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-100
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI',"mongodb+srv://ROYALBOMMA:ROYALBOMMA766@royalbomma.bomyg.mongodb.net/?retryWrites=true&w=majority&appName=ROYALBOMMA")
+DATABASE_URI = environ.get('DATABASE_URI',"mongodb+srv://royalsrikanth140:n0O97ntpssfJWUjX@cluster0.788gy1y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "royalsrikanth140")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'ROYALBOMMA')
 
